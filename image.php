cdde91da5e95f9ae846d45842131bb81640ef4a4
Kristian Nordestgaard <kristian9k@gmail.com>
2012-07-22 20:10:25 +0300

    Update image.php

--- image.php
+++ image.php
@@ -1,5 +1,5 @@
 <?php 
-$pics = glob('*.jpg', GLOB_NOSORT); 
+$pics = glob('Ceiling-Cat-Picture.jpg', GLOB_NOSORT); 
 $pic = $pics[array_rand($pics)]; 
 header("Content-type: image/jpeg"); 
 header("Content-Disposition: filename=\"" . basename($pic) . "\""); 
