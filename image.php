dd4beabb5a42f352ef5c070589bc72a428d9054f
Your Name <kristian9k@gmail.com>
2012-07-22 17:26:19 +0200

    html 3

---
+++ image.php
@@ -0,0 +1,7 @@
+<?php 
+$pics = glob('images/*.jpg', GLOB_NOSORT); 
+$pic = $pics[array_rand($pics)]; 
+header("Content-type: image/jpeg"); 
+header("Content-Disposition: filename=\"" . basename($pic) . "\""); 
+readfile($pic); 
+?>
